d19d22bceece821759675ac513770b061e694efc
Mikhaïl Hanon <mike.hanon@gmail.com>
2019-09-22 16:15:11 +0200

    modif timer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var timer = 30;
-var countdown= setInterval(bonus30s, 1000)
+var countdown= setInterval(timer, 1000)
 
-function bonus30s (){
+function timer (){
     if(timer == -1){
         clearTimeout(countdown);
         alert('test')
