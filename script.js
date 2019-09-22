cf492e4b70dad8e70b71651552411a5e4c14717e
Mikhaïl Hanon <mike.hanon@gmail.com>
2019-09-22 19:19:41 +0200

    timer ready

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var timer = 30;
 var countdown= setInterval(timer, 1000)
-
-function timer (){
-    if(timer == -1){
-        clearTimeout(countdown);
-        alert('test')
-    }else{
-    document.getElementById('chrono').innerHTML = timer +' S'
-    timer --;
+var bonus = document.getElementById('chrono')
+bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+bonus.addEventListener("click", function(){
+var countdown = setInterval(function(){
+    timer -- 
+    bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+    if(timer == 0){
+        bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+        clearInterval(countdown)
+        timer=30
+        bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
     }
-}
+}, 1000);
+});
