590bb9fbb9b2684c95ea732a0a401eab2658c7cf
a-o013 <ayoub-ochan@hotmail.com>
2019-09-23 22:38:00 +0200

    motion

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-var timer = 30;
-var bonus = document.getElementById('chrono')
-bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-bonus.addEventListener("click", function(){
-var countdown = setInterval(function(){
-    timer -- 
-    bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-    if(timer == 0){
-        bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-        clearInterval(countdown)
-        timer=30
-        bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-    }
-}, 1000);
-});
+const canvas = document.getElementById('canvas')
+const child = document.getElementById('child')
+
+const motion = () => {
+    const animations = [1, 2, 3, 4]
+    setInterval(() => {
+        let animation = animations[Math.round(Math.random()*3)]
+        canvas.style.animation = `xAxis${animation} 3000ms linear`
+        child.style.animation = `yAxis${animation} 3000ms ease-in-out`
+    }, 3000)
+}
+motion()
