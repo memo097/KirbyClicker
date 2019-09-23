f182d95ddb16208896152045601f9ff0bcfd14be
memo097 <ozcan.mehmet@outlook.fr>
2019-09-23 14:36:19 +0200

    game over

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var life = 10;
 var timer = 30;
 var bonus = document.getElementById('chrono')
 bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
@@ -13,3 +14,21 @@ var countdown = setInterval(function(){
     }
 }, 1000);
 });
+document.querySelector(".main-container").onclick=(e)=>gameover(e)
+document.querySelector(".game-life").innerHTML = life
+function gameover(e){
+    if(!e.path[0].id.includes("canvas")){
+        life--
+        console.log(life)
+        if(life==0){
+            document.querySelector(".game-over").style.animation="gameOver 1s ease-in forwards"
+            document.querySelector(".gif-retry").style.animation="gifRetry 4s ease-in forwards 1s"
+            document.querySelector(".gif-retry button").style.animation="buttonRetry 4s ease-in 1s"
+            document.querySelector(".main-container").onclick=(e)=false
+
+        }
+    }else{
+        console.log(life)
+    }
+    document.querySelector(".game-life").innerHTML = life
+}
