9ce7be47bee81f36e735f31ceaa3f59f846b3829
Samuel <Sam_reyniers@hotmail.com>
2019-09-23 15:32:40 +0200

    Score

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 var timer = 30;
+var score = 0;
+var numbers = document.getElementById("numbers");
+var cookie = document.getElementById("cookie");
+var multiplier = 1;
+
 var bonus = document.getElementById('chrono')
 bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-bonus.addEventListener("click", function(){
-var countdown = setInterval(function(){
-    timer -- 
-    bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-    if(timer == 0){
+bonus.addEventListener("click", function () {
+    var countdown = setInterval(function () {
+        timer--
         bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-        clearInterval(countdown)
-        timer=30
-        bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-    }
-}, 1000);
+        if (timer == 0) {
+            bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+            clearInterval(countdown)
+            timer = 30
+            bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+        }
+    }, 1000);
 });
+
+
+
+function cookieClick() {
+    console.log(score)
+   score = score + (1*multiplier);
+   
+numbers.textContent = score ;
+}
+function fire () {
+    
+    multiplier = 2; 
+}
+function water () {
+   
+    multiplier = 10;
+}
+function frost () {
+   
+    multiplier = 20;
+}
+function rainbow () {
+   
+    multiplier = 100;
+}
