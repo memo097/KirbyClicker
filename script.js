2f2432091c24173e52660bf209ff995bd79d4233
Mikhaïl Hanon <mike.hanon@gmail.com>
2019-09-23 12:09:43 +0200

    ajout prix

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-var timer = 30;
+var timer = 10;
 var bonus = document.getElementById('chrono')
-var bonusclick = 0
+
 bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+if(score < 5000 ){
+    bonus.disabled = true
+}else if (score >= 5000){
+    bonus.disabled = false
+}
+console.log(score, 1)
 bonus.addEventListener("click", function(){
     bonus.disabled = true
+    score = score - 5000
 var countdown = setInterval(function(){
     timer -- 
     bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
     if(timer == 0){
         bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
         clearInterval(countdown)
-        bonus.disabled = false
-        timer=30
+        if(score < 5000 ){
+            bonus.disabled = true
+        }else if (score >= 5000){
+            bonus.disabled = false
+        }
+        timer=10
         bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
     }
 }, 1000);
+
 });
