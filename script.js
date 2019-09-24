6956dd5c6e6e6e4d8bfec6d5b99a62aed4167774
Samuel <Sam_reyniers@hotmail.com>
2019-09-24 09:21:42 +0200

    Break

--- script.js
+++ script.js
@@ -1,82 +1,104 @@
 var timer = 30;
 var score = 0;
 var numbers = document.getElementById("numbers");
-var cookie = document.getElementById("bigCanvas");
+var bigCanvas = document.getElementById("bigCanvas");
 var multiplier = 1;
 
 function cookieClick() {
     console.log(score)
-   score = score + (1*multiplier);
-   
-numbers.textContent = score ;
+    score = score + (1 * multiplier);
+
+    numbers.textContent = score;
+    if (score < 5000) {
+        bonus.disabled = true
+    } else if (score >= 5000) {
+        bonus.disabled = false
+    }
 }
-function fire () {
-    
-    multiplier = 2; 
+if (score < 30) {
+    document.querySelector('.bottom-fire').disabled = true
+} else if (score >= 30) {
+    document.querySelector('.bottom-fire').disabled = false
 }
-function water () {
-   
+if (score < 500) {
+    document.querySelector('.bottom-water').disabled = true
+} else if (score >= 500) {
+    document.querySelector('.bottom-water').disabled = false
+}
+if (score < 1000) {
+    document.querySelector('.bottom-frost').disabled = true
+} else if (score >= 1000) {
+    document.querySelector('.bottom-frost').disabled = false
+}
+if (score < 10000) {
+    document.querySelector('.bottom-rainbow').disabled = true
+} else if (score >= 10000) {
+    document.querySelector('.bottom-rainbow').disabled = false
+}
+function fire() {
+
+    multiplier = 2;
+}
+function water() {
+
     multiplier = 10;
 }
-function frost () {
-   
+function frost() {
+
     multiplier = 20;
 }
-function rainbow () {
+function rainbow() {
    
     multiplier = 100;
-
 }
+
+
 
 var life = 10;
 
 var bonus = document.getElementById('chrono')
 bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-if(score < 5000 ){
-    bonus.disabled = true
-}else if (score >= 5000){
-    bonus.disabled = false
-}
+
 console.log(score, 1)
-bonus.addEventListener("click", function(){
+bonus.addEventListener("click", function () {
     bonus.disabled = true
     score = score - 5000
-    multiplier = multiplier*2
-var countdown = setInterval(function(){
-    timer -- 
-    bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-    if(timer == 0){
+    multiplier = multiplier * 2
+    var countdown = setInterval(function () {
+        timer--
         bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-        clearInterval(countdown)
-        if(score < 5000 ){
-            bonus.disabled = true
-        }else if (score >= 5000){
-            bonus.disabled = false
+        if (timer == 0) {
+            bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+            clearInterval(countdown)
+            if (score < 5000) {
+                bonus.disabled = true
+            } else if (score >= 5000) {
+                bonus.disabled = false
+            }
+            timer = 10
+            bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
+            multiplier = multiplier / 2
         }
-        timer=10
-        bonus.innerHTML = "bonus mutiplicateur X2 " + timer + " Seconde"
-        multiplier = multiplier/2
-    }
-}, 1000);
+    }, 1000);
 });
 
-document.querySelector(".main-container").onclick=(e)=>gameover(e)
+document.querySelector(".main-container").onclick = (e) => gameover(e)
 document.querySelector(".game-life").innerHTML = life
-function gameover(e){
-    if(!e.path[0].id.includes("bigCanvas")){
+function gameover(e) {
+    if (!e.path[0].id.includes("bigCanvas")) {
         life--
-        if(life==0){
-            document.querySelector(".game-over").style.animation="gameOver 1s ease-in forwards"
-            document.querySelector(".gif-retry").style.animation="gifRetry 4s ease-in forwards 1s"
-            document.querySelector(".gif-retry button").style.animation="buttonRetry 4s ease-in 1s"
-            document.querySelector(".main-container").onclick=(e)=false
+        if (life == 0) {
+            document.querySelector(".game-over").style.animation = "gameOver 1s ease-in forwards"
+            document.querySelector(".gif-retry").style.animation = "gifRetry 4s ease-in forwards 1s"
+            document.querySelector(".gif-retry button").style.animation = "buttonRetry 4s ease-in 1s"
+            document.querySelector(".main-container").onclick = (e) = false
 
         }
-    }else{
+    } else {
         console.log(life)
     }
     document.querySelector(".game-life").innerHTML = life
 }
-function tryAgain(){
+function tryAgain() {
     document.location.reload()
 }
