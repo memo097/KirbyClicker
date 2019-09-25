d43659c44b8dc38f68cc55f15fefdd23fd9f0749
memo097 <ozcan.mehmet@outlook.fr>
2019-09-25 10:42:27 +0200

    bonusbutton

--- script.js
+++ script.js
@@ -191,9 +191,11 @@ function gameover(e) {
         canvas()
         if(score < 200 || autoactif) document.getElementById("autoclick").disabled = true
         else document.getElementById("autoclick").disabled = false
+        if(parseInt(currentScore.textContent) > 10) document.getElementById("autoclick").disabled = true
         if (score < 5 ) {document.getElementById("bonusclick").disabled = true; console.log(55)}
         else document.getElementById("bonusclick").disabled = false
         if (bonusclick === 600) document.getElementById("bonusclick").disabled = true
+        if(parseInt(currentScore.textContent) > 10) document.getElementById("bonusclick").disabled = true
 
         if (score >= 10) document.getElementById("buyLifeButton").disabled = false;
         else document.getElementById("buyLifeButton").disabled = true;
